4fe744bf3c510ea375ead3459008bab57109c6dc
QsemkiQ <sem.semki@mail.ru>
2026-05-10 15:33:24 +0700

    Update dino.lua

--- dino.lua
+++ dino.lua
@@ -1,6 +1,6 @@
 local speaker = peripheral.find("speaker")
 if not speaker then
-    error("Динамик не найден!")
+    error("NO dio")
 end
 
 local dfpwm = require("cc.audio.dfpwm")
@@ -8,7 +8,7 @@ local decoder = dfpwm.make_decoder()
 
 local file = io.open("1.dfpmu", "rb")
 if not file then
-    error("Файл 1.dfpmu не найден!")
+    error("NO 1.dfpmu!")
 end
 
 local data = file:read("*a")
