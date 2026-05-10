171b25941bc0014fec5d5590289251f2b4a837d7
QsemkiQ <sem.semki@mail.ru>
2026-05-10 18:54:40 +0700

    Update dino.lua

--- dino.lua
+++ dino.lua
@@ -1,28 +1,18 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
-
-if not speaker then
-    error("No speaker attached")
-end
+if not speaker then error("Speaker not found") end
 
 local decoder = dfpwm.make_decoder()
-local file = io.open("data/example.dfpwm", "rb")  -- "rb" = read binary
-
-if not file then
-    error("File not found")
-end
+local file = io.open("data/123.dfpwm", "rb")
+if not file then error("The file was not found. Way: " .. shell.getRunningProgram()) end
 
 while true do
-    local chunk = file:read(16 * 1024)  -- Читаем 16KB бинарных данных
-    if not chunk then break end          -- Конец файла
-    
-    local buffer = decoder(chunk)        -- Декодируем в PCM
-    
-    -- Отправляем в динамик, ожидая освобождения
+    local chunk = file:read(16 * 1024)
+    if not chunk then break end
+    local buffer = decoder(chunk)
     while not speaker.playAudio(buffer) do
         os.pullEvent("speaker_audio_empty")
     end
 end
-
 file:close()
-print("Playback finished")
+print("Playback is complete!")
